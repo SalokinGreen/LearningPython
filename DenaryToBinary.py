e949415145261582fbb3c4b05f0661fb09d8a307
SalokinGreen <50830205+SalokinGreen@users.noreply.github.com>
2019-05-25 19:05:33 +0200

    3Day: Second program
    Much better! I needed 2 minutes after the first try with only if.

---
+++ DenaryToBinary.py
@@ -0,0 +1,10 @@
+print("Welcome to my second program! Again a denary to binary calculator!")
+
+number = int(input ("Decimal:") )
+
+while number > 0 :
+    print(str(int(number % 2)))
+    number = int(number / 2)
+    if number == 0 :
+        break
+    
