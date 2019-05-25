cd3544aeb17ebd14be6d3259816bb5812095f79e
SalokinGreen <50830205+SalokinGreen@users.noreply.github.com>
2019-05-25 19:15:40 +0200

    Update DenaryToBinary.py

--- DenaryToBinary.py
+++ DenaryToBinary.py
@@ -1,10 +1,11 @@
 print("Welcome to my second program! Again a denary to binary calculator!")
 
-number = int(input ("Decimal:") )
+while True:
+    number = int(input ("Decimal:") )
 
-while number > 0 :
-    print(str(int(number % 2)))
-    number = int(number / 2)
-    if number == 0 :
-        break
+    while number > 0 :
+        print(str(int(number % 2)))
+        number = int(number / 2)
+        if number == 0 :
+            break
     
